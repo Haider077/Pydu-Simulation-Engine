8e4ed7a266c571f60a5b0b364e957833d5378236
Hasib Amin <haideramin077@gmail.com>
2021-10-23 22:09:29 -0700

    initial

---
+++ AEngineCore.js
@@ -0,0 +1,29 @@
+setRendererBackgroundColor("#92cfd3");
+render();
+var obj = createCubeObject(10,1,10,"#265e31");
+setObjectPosition(obj,1,-2,-10);
+setCameraPosition(0,0,15);
+
+const W_KEY = 87;
+const A_KEY = 65;
+const S_KEY = 83;
+const D_KEY = 68;
+
+
+
+window.addEventListener("keydown",function globalInputEvents(event){
+
+    if(event == W_KEY){
+        
+    }
+    if(event == A_KEY){
+
+    }
+    if(event == S_KEY){
+
+    }
+    if(event == D_KEY){
+
+    }
+
+});
